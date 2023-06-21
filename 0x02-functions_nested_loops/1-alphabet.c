abe2100fd99f4c9fb3b4cc957b1f1e94b76eae30
cherifBurette1 <76395084+cherifBurette1@users.noreply.github.com>
2023-06-21 04:41:20 +0300

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,21 +1,20 @@
-#include main.h 
-  
- /** 
-  * print_alphabet - check the code for ALX School students. 
-  * 
-  * Return: Always 0. 
-  */ 
-  
- void print_alphabet(void) 
- { 
-         char alp = 'a'; 
-  
-         while (alp <= 'z') 
-         { 
-                 _putchar (alp); 
-                 alp++; 
-  
-         } 
-         _putchar ('\n'); 
-  
- }
+#include "main.h"
+
+/**
+ * print_alphabet - Entry point
+ * Return:0
+ */
+
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+
+	}
+	_putchar('\n');
+
+}
