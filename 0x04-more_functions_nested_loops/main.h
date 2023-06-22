9ccf6005329df796bc0641c39a39698c4d7723f5
cherifBurette1 <76395084+cherifBurette1@users.noreply.github.com>
2023-06-23 02:38:05 +0300

    Update main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,16 +1,18 @@
-#ifndef Main_H 
- #define Main_H 
+#ifndef MAIN_H 
+#define MAIN_H 
   
- void positive_or_negative(int i); 
- int largest_number(int a, int b, int c); 
- int convert_day(int month,int day); 
- void print_remaining_days(int month,int day,int year); 
- void print_to_98(int n); 
+ /* 
+  * File: main.h 
+  * Auth: Ephrem Abebe 
+  * Desc: Header file containing prototypes for all functions 
+  *       used in the 0x03-more_functions_nested_loops directory. 
+  */ 
+  
+ int _putchar(char c); 
  int _isupper(int c); 
  int _isdigit(int c); 
  int mul(int a, int b); 
  void print_numbers(void); 
- int _putchar(char c); 
  void print_most_numbers(void); 
  void more_numbers(void); 
  void print_line(int n); 
