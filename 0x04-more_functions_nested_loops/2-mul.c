5dc46cce13e1db8583d81ac1e79b945dc098d85c
cherifBurette1 <76395084+cherifBurette1@users.noreply.github.com>
2023-06-23 04:41:24 +0300

    Update 2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,18 +1,12 @@
-/* 
-  * File: 2-mul.c 
-  * Auth: sherif
-  */ 
-  
- #include "main.h" 
-  
- /** 
-  * mul - Multiples two integers. 
-  * @a: The first integer to be multiplied. 
-  * @b: The second integer to be multiplied. 
-  * 
-  * Return: The result of the multiplication. 
-  */ 
- int mul(int a, int b) 
- { 
-         return (a * b); 
- }
+#include "main.h"
+/**
+*mul - function that multipliestwo numbers
+*@a: first number
+*@b: second number
+*Return: returns result
+*/
+
+int mul(int a, int b)
+{
+return (a * b);
+}
