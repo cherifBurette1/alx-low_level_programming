d4e6886798174a95ab757714647cb9e69b7b4ae9
cherifBurette1 <76395084+cherifBurette1@users.noreply.github.com>
2023-06-26 06:56:14 +0300

    Update main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,21 +1,17 @@
-#include "main.h" 
+#ifndef main 
   
- /** 
-  * puts_half - prints the second half of a string. 
-  *@str: string to use. 
-  */ 
+ int _putchar(char c); 
+ void reset_to_98(int *n); 
+ void swap_int(int *a, int *b); 
+ int _strlen(char *s); 
+ void _puts(char *str); 
+ void print_rev(char *s); 
+ void rev_string(char *s); 
+ void puts2(char *str); 
+ void puts_half(char *str); 
+ void print_array(int *a, int n); 
+ char *_strcpy(char *dest, char *src); 
+ int _atoi(char *s); 
   
- void puts_half(char *str) 
- { 
-         int length, n, i; 
-  
-         for (length = 0; str[length] != '\0'; length++) 
-         { 
-         } 
-         n = (length - 1) / 2; 
-         for (i = n + 1; str[i] != '\0'; i++) 
-         { 
-                 _putchar(str[i]); 
-         } 
-         _putchar('\n'); 
- }
+ #define main 
+ #endif
